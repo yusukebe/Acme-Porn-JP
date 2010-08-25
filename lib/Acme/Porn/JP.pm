c0878cdcbe4cc47614b45443e994c0a90a5431b7
Yusuke Wada <yusukebe@cpan.org>
2010-08-25 20:31:00 +0900

    pod.

--- lib/Acme/Porn/JP.pm
+++ lib/Acme/Porn/JP.pm
@@ -43,12 +43,18 @@ Acme::Porn::JP - Porn terms in Japan.
   use Acme::Porn::JP;
 
   my $porn_jp = Acme::Porn::JP->new();
+
   my $actress_list = $porn_jp->actress();
   print $actress_list->[0] . "\n";
+
+  my $category_list = $porn_jp->category();
+  print $category_list->[10] . "\n";
 
 =head1 DESCRIPTION
 
 Acme::Porn::JP is term list about japanese porn a.k.a Adult Video .
+
+Available method are 'actress' and 'category'.
 
 =head1 AUTHOR
 
