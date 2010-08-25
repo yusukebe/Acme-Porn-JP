aff35d15e677a69a590143a735b7fb9cb1a86cff
Yusuke Wada <yusukebe@cpan.org>
2010-08-25 19:40:32 +0900

    initial commit

---
+++ lib/Acme/Porn/JP.pm
@@ -0,0 +1,32 @@
+package Acme::Porn::JP;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Acme::Porn::JP -
+
+=head1 SYNOPSIS
+
+  use Acme::Porn::JP;
+
+=head1 DESCRIPTION
+
+Acme::Porn::JP is
+
+=head1 AUTHOR
+
+Yusuke Wada E<lt>yusuke at kamawada.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
